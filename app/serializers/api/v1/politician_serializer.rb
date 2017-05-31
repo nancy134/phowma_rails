ed06a7ad4fbdb4f69438dc1f46c395153879d7d3
Nancy Piedra <nancy_piedra@yahoo.com>
2017-05-31 11:43:55 +0000

    Add politician picture

--- app/serializers/api/v1/politician_serializer.rb
+++ app/serializers/api/v1/politician_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::PoliticianSerializer < Api::V1::BaseSerializer
-  attributes :id, :first_name, :last_name, :party, :position
+  attributes :id, :first_name, :last_name, :party, :position, :picture
 end
