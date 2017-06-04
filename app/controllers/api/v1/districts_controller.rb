1c7c7ccbfd2a0e945c1d0719426354bf9d137fbe
Nancy Piedra <nancy_piedra@yahoo.com>
2017-06-04 11:04:20 +0000

    districts/find api

--- app/controllers/api/v1/districts_controller.rb
+++ app/controllers/api/v1/districts_controller.rb
@@ -11,10 +11,14 @@ class Api::V1::DistrictsController < Api::V1::BaseController
         render json: districts, each_serializer: Api::V1::DistrictSerializer
     end
     def find
-    
+        output = {:message => "Not enough address data to determine congressional district"}
         #Use the State to determine District
         if (params[:state])
-            state = Admins::State.where(abbreviation: params[:state]).first
+            if (params[:state].length == 2)
+              state = Admins::State.where(abbreviation: params[:state]).first
+            else
+              state = Admins::State.where(name: params[:state]).first
+            end
             districts = Admins::District.where(state_id: state.id)
             if (districts.length == 1)
                 output = {:district => districts[0].name, :state => state.abbreviation}
@@ -76,25 +80,30 @@ class Api::V1::DistrictsController < Api::V1::BaseController
                 faraday.adapter Faraday.default_adapter
             end
             response = conn.get '/civicinfo/v2/representatives', { :key => 'AIzaSyCnf229S5uGfG2gRpnE6npbHXZZgOxOyNo', :address => query, :includeOffices => 'false', :roles => 'legislatorLowerBody' }
+            Rails.logger.debug "NANCY: response.body: #{response.body}"
             repo_info = JSON.parse(response.body)
             if (repo_info['divisions'])
                 if (repo_info['divisions'].keys.first)
-                    splits = repo_info['divisions'].keys.first.split(':')
+                    Rails.logger.debug "NANCY: keys.first: #{repo_info['divisions'].keys.first}"
+                    key = repo_info['divisions'].keys.first
+                    splits =key.split(':')
+                    Rails.logger.debug "NANCY: splits.length: #{splits.length}"
                     if (splits.length == 4)
-                        district_record = Admins::District.where(number: splits[3])
+                        state_record = Admins::State.where(abbreviation: params[:state]).first
+                        district_record = Admins::District.where(number: splits[3], state_id: state_record.id).first
                         state = ""
                         if (district_record) 
                             state = district_record.state.abbreviation
                         end
                         render json: { :district => splits[3], :state => state} and return
                     else
-                        render json: { :message => 'Could not find district for this address'} and return
+                        render json: { :message => 'Could not find district for this address[1]'} and return
                     end
                 else 
-                    render json: { :message => 'Could not find district for this address'} and return
+                    render json: { :message => 'Could not find district for this address[2]'} and return
                 end
             else
-                render json: { :message => 'Could not find district for this address'} and return
+                render json: { :message => 'Could not find district for this address[3]'} and return
             end 
             output = {:message => 'Cannot determine district with just state information as this state has multiple districts'}
             render json: output and return
