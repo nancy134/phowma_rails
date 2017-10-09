026519a6c754b45dc5d6339c015bc5a9023590c8
Nancy Piedra <nancy_piedra@yahoo.com>
2017-10-09 20:31:48 +0000

    Database deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:nancy134/phowma_rails.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/ubuntu/deploys/phowma_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -34,3 +34,9 @@ set :repo_url, "git@github.com:nancy134/phowma_rails.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :use_sudo, true
+set :branch, 'master' #or whichever branch you want to use
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_files, %w(config/application.yml)
+
