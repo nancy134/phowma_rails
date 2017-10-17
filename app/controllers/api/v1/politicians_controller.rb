e2c0a197dc87ebf89c479822346bb7412a51df5f
Nancy Piedra <nancy_piedra@yahoo.com>
2017-10-17 11:55:45 +0000

    Add ransack to politician api

--- app/controllers/api/v1/politicians_controller.rb
+++ app/controllers/api/v1/politicians_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::PoliticiansController < Api::V1::BaseController
   def index
+
+    
     if (params[:state])
       if (params[:state].length == 2)
         state = Admins::State.where(abbreviation: params[:state]).first
@@ -10,11 +12,9 @@ class Api::V1::PoliticiansController < Api::V1::BaseController
         politicians = Admins::Politician.where(state_id: state.id)
       end
       render json: politicians, each_serializer: Api::V1::PoliticianSerializer
-    else 
-      #politicians = Admins::Politician.all
-     # politicians = Admins::Politician.includes(:admins_state).order("admins_state.name desc")
-      #politicians = Admins::Politician.find(:all, :joins => :state, :order => 'admins_states.name')
-      politicians = Admins::Politician.joins(:state).order("admins_states.name")
+    else
+      rpoliticians = Admins::Politician.ransack(params[:q])
+      politicians = rpoliticians.result.joins(:state).order("admins_states.name")
       paginate json: politicians, each_serializer: Api::V1::PoliticianSerializer
     end
   end
