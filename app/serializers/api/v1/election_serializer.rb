faca3564d7063b8e03d9bac07c5def77b3ee2a5c
Nancy Piedra <nancy_piedra@yahoo.com>
2018-03-05 20:47:42 +0000

    Add politician to election api

--- app/serializers/api/v1/election_serializer.rb
+++ app/serializers/api/v1/election_serializer.rb
@@ -2,4 +2,5 @@ class Api::V1::ElectionSerializer < Api::V1::BaseSerializer
   attributes :election_name, :date, :position
   belongs_to :state, serializer: Api::V1::StateMinSerializer
   belongs_to :district
+  belongs_to :politician, serializer: Api::V1::PoliticianSerializer
 end
