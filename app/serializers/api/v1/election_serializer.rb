74dc234e8ce6a74575bed7cd48fe9b2b6862a51a
Nancy Piedra <nancy_piedra@yahoo.com>
2018-03-05 16:28:02 +0000

    Add district to elections

--- app/serializers/api/v1/election_serializer.rb
+++ app/serializers/api/v1/election_serializer.rb
@@ -1,4 +1,5 @@
 class Api::V1::ElectionSerializer < Api::V1::BaseSerializer
   attributes :election_name, :date, :position
-  belongs_to :state
+  belongs_to :state, serializer: Api::V1::StateMinSerializer
+  belongs_to :district
 end
