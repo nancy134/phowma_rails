c093def814ed198acf6699cb7e859326f920e7e9
Nancy Piedra <nancy_piedra@yahoo.com>
2018-05-01 18:19:30 +0000

    Add map coord

--- app/serializers/api/v1/district_serializer.rb
+++ app/serializers/api/v1/district_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::DistrictSerializer < Api::V1::BaseSerializer
-  attributes :id, :name, :number
+  attributes :id, :name, :number, :longitude1, :latitude1, :longitude2, :latitude2
   has_one :politician, serializer: Api::V1::PoliticianMinSerializer
   belongs_to :state, serializer: Api::V1::StateMinSerializer
 end
