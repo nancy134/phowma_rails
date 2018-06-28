bc7eef47e251b4bb808d7a245251137d88fd124d
Nancy Piedra <nancy_piedra@yahoo.com>
2018-06-28 02:07:07 +0000

    Fix Rack:Cors

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module PhowmaRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://server.phowma.com'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
