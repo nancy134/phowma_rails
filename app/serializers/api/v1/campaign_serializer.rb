3c843e0d4e70aa7ce6752f1fff5118c0369be6ab
Nancy Piedra <nancy_piedra@yahoo.com>
2018-08-22 14:15:05 +0000

    Campaign api fix

--- app/serializers/api/v1/campaign_serializer.rb
+++ app/serializers/api/v1/campaign_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::CampaignSerializer < Api::V1::BaseSerializer
   attributes :id
   belongs_to :election, serializer: Api::V1::ElectionSerializer
-  belongs_to :politician, serializer: Api::V1::PoliticianMinSerializer
+  belongs_to :politician, serializer: Api::V1::PoliticianSerializer
 end
