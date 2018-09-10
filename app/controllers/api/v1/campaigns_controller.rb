c1086a0b97885c6497ad1ad7a11c1bd54db2871e
Nancy Piedra <nancy_piedra@yahoo.com>
2018-09-10 10:58:18 +0000

    bug fix

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CampaignsController < Api::V1::BaseController
   def index
     campaigns = Admins::Campaign.ransack(params[:q])
-    json: campaigns.result, each_serializer: Api::V1::CampaignSerializer, include: ['election', 'election.office.state', 'election.office.district', 'politician.posts']
+    render json: campaigns.result, each_serializer: Api::V1::CampaignSerializer, include: ['election', 'election.office.state', 'election.office.district', 'politician.posts']
   end
 end
